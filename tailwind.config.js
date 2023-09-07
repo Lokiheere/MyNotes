7ecdc7c33fad3902c40625865a4e5d3e4404cb09
Lokiheere <bonxp2@gmail.com>
2023-09-07 12:59:52 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    content: [
+      "./src/MyNote/templates/**/*.{html, js}",  // All HTML files in MyNote/templates and its subdirectories
+      "./src/**/*.js"                       // All JavaScript files in src and its subdirectories
+  ],
+  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
